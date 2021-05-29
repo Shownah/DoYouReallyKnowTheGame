ab55b644e9b87cf19d1cf1a34fe27a8d19f8ca96
Jarod <jarod.acloque@live.fr>
2021-05-29 19:29:39 +0200

    Quizz v1

--- app/src/main/java/doyoureally/knowthegame/Game/ListGames.java
+++ app/src/main/java/doyoureally/knowthegame/Game/ListGames.java
@@ -24,12 +24,12 @@ public class ListGames {
         listG.add(new Game("GTFO", mipmap.gtfo));
         listG.add(new Game("League of Legends", mipmap.lol));
         listG.add(new Game("Super Smash Bros. Ultimate", mipmap.smash));
-        listG.add(new Game("Monster Hunter Rise", mipmap.mhr));
-        listG.add(new Game("Biomutant", mipmap.biomutant));
+        // listG.add(new Game("Monster Hunter Rise", mipmap.mhr));
+        // listG.add(new Game("Biomutant", mipmap.biomutant));
         listG.add(new Game("GTA V", mipmap.gta));
-        listG.add(new Game("The Binding of Isaac", mipmap.isaac));
-        listG.add(new Game("Pummel Party", mipmap.pummel_party));
-        listG.add(new Game("Resident Evil Village", mipmap.re_village));
+        // listG.add(new Game("The Binding of Isaac", mipmap.isaac));
+        // listG.add(new Game("Pummel Party", mipmap.pummel_party));
+        // listG.add(new Game("Resident Evil Village", mipmap.re_village));
         listG.add(new Game("Sea Of Thieves", mipmap.sot));
     }
 }
